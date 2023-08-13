1c1f07e2efbccf94706b7867cd694643efa47fe7
geekyBoogiepop <fljustsocial@outlook.com>
2023-08-13 16:26:00 -0600

    Initial commit

---
+++ mydb.py
@@ -0,0 +1,18 @@
+# pip install mysql
+# pip install mysql-connector
+# pip install mysql-connector-python
+
+import mysql.connector
+
+dataBase = mysql.connector.connect(
+    host="localhost",
+    user="faustino",
+    passwd="#!faustino",
+)
+
+# prepare a cursor object
+cursorObject = dataBase.cursor()
+
+# Create a database
+cursorObject.execute("CREATE DATABASE elderco")
+print("All done!")
